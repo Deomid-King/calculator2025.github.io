1d67b2cd008b5533ed45f40256d00a3f43ecaa99
Deomid-King <deomid555@gmail.com>
2025-01-17 17:09:33 +0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,7 +4,7 @@ function calculate() {
 	var result = document.getElementById("result").value;
 
 	// рассчитываем результат
-	var output = +price * 0.84 - +result - (weight == "5t" ? 100: weight == "15t" ? 899 : weight == "5kg" ? 1799 : weight == "15kg" ? 2199 : weight == "50kg" ? 3999 : 6999);
+	var output = +price * 0.84 - +result - (weight == "5t" ? 100: weight == "15t" ? 899 : weight == "5kg" ? 1799 : weight == "15kg" ? 2199 : weight == "50kg" ? 4099 : 6999);
 
 	// выводим результат на страницу
 	document.getElementById("output").innerHTML = "Маржа: " + Math.round(output);
