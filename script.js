f675f018df4ec9328cdf211d609afd079dc030e9
Deomid-King <deomid555@gmail.com>
2025-02-21 09:01:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calculate() {
 	var result = document.getElementById("result").value;
 
 	// рассчитываем результат
-	var output = (+price + +result +(weight == "5t" ? 100: weight == "15t" ? 995 : weight == "5kg" ? 1955 : weight == "15kg" ? 2403 : weight == "50kg" ? 4531 : 7779)) / 0.82;
+	var output = (+price + +result +(weight == "5t" ? 100: weight == "15t" ? 995 : weight == "5kg" ? 1955 : weight == "15kg" ? 2403 : weight == "50kg" ? 4531 : 7779)) / 0.83;
 
 	// выводим результат на страницу
 	document.getElementById("output").innerHTML = "Итоговая цена для Каспи: " + Math.round(output);
