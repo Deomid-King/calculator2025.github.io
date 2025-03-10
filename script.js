2d0f33be8871a39a004965167e4c798e7e5efdeb
Deomid-King <deomid555@gmail.com>
2025-03-10 11:10:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ function calculate() {
 	var price = document.getElementById("price").value;
 	var weight = document.getElementById("weight").value;
 	var result = document.getElementById("result").value;
+	var procent = document.getElementById("procent").value;
 
 	// рассчитываем результат
 	var output = (+price + +result +(weight == "5t" ? 100: weight == "15t" ? 995 : weight == "5kg" ? 1955 : weight == "15kg" ? 2403 : weight == "50kg" ? 4531 : 7779)) / 0.83;
+
+	// расчет процентов
+	var procent = (+result / +price) * 100;
+
+	// выводим результат на страницу
+	document.getElementById("procent").innerHTML = Math.round(procent) + "%";
 
 	// выводим результат на страницу
 	document.getElementById("output").innerHTML = "Итоговая цена для Каспи: " + Math.round(output);
