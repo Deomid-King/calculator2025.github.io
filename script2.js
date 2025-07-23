76f001d24a08d99320187abe0da83ed1d44356d0
Deomid-King <deomid555@gmail.com>
2025-07-23 10:12:39 +0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -5,7 +5,7 @@ function calculate() {
 	var procent = document.getElementById("procent").value;
 	
 	// рассчитываем результат
-	var output = +price * 0.83 - +result - (weight == "5t" ? 100: weight == "15t" ? 1195 : weight == "5kg" ? 2255 : weight == "15kg" ? 2703 : weight == "50kg" ? 4831 : 8079);
+	var output = +price * 0.83 - +result - (weight == "5t" ? 150: weight == "15t" ? 1195 : weight == "5kg" ? 2255 : weight == "15kg" ? 2703 : weight == "50kg" ? 4831 : 8079);
 
 	// расчет процентов
 	var procent = (+output / +result) * 100;
